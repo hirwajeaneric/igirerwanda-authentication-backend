1ff80e0060edb4162a22dc8af9bb90f78e850e7f
HIRWA Jean Eric <hirwaminerve25@gmail.com>
2025-01-05 10:30:40 +0200

    ft:Validation
    I added validation using express validator package

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,4 +1,5 @@
 import AuthService from "../services/auth-service";
+import { validateUserSignUp, validateUserSignIn, validatePasswordReset } from "../utils/auth.validation";
 import asyncWrapper from "./middlewares/AsyncWrapper";
 import { Authorize } from "./middlewares/auth";
 import { Request, Response, NextFunction, Application } from "express";
@@ -6,54 +7,53 @@ import { Request, Response, NextFunction, Application } from "express";
 export default (app: Application) => {
     const authService = new AuthService();
 
-    app.get('/health', asyncWrapper(async (req: Request, res: Response, next: NextFunction) => {
-        res.status(200).json("Health is good");
+    app.get('/health', asyncWrapper(async (req: Request, res: Response) => {
+        res.status(200).json({ message: "Health is good" });
     }));
 
-    app.get('/user/list', Authorize, asyncWrapper(async (req: Request, res: Response, next: NextFunction) => {
+    app.get('/list', Authorize, asyncWrapper(async (req: Request, res: Response) => {
         const users = await authService.getAllUsers();
         res.status(200).json({ users });
     }));
 
-    app.get('/user/findById', Authorize, asyncWrapper(async (req: Request, res: Response, next: NextFunction) => {
+    app.get('/findById', Authorize, asyncWrapper(async (req: Request, res: Response) => {
         const user = await authService.getUserById(req.query.id as string);
         res.status(200).json({ user });
     }));
 
-    app.get('/users/findByEmail', Authorize, asyncWrapper(async (req: Request, res: Response, next: NextFunction) => {
+    app.get('/findByEmail', Authorize, asyncWrapper(async (req: Request, res: Response) => {
         const user = await authService.getUserByEmail(req.query.email as string);
         res.status(200).json({ user });
     }));
 
-    app.get('/user/profile', Authorize, asyncWrapper(async (req: Request, res: Response, next: NextFunction) => {
+    app.get('/profile', Authorize, asyncWrapper(async (req: Request, res: Response) => {
         const result = await authService.getProfile(req.query.id as string, req.query.appId as string);
         res.status(200).json({ user: result});
     }));
 
-    app.post('/user/signup', asyncWrapper(async (req: Request, res: Response, next: NextFunction) => {
+    app.post('/signup', validateUserSignUp, asyncWrapper(async (req: Request, res: Response) => {
         const result = await authService.signup(req.body);
         res.status(201).json(result);
     }));
 
-    app.post('/user/login', asyncWrapper(async (req: Request, res: Response, next: NextFunction) => {
+    app.post('/signin', validateUserSignIn, asyncWrapper(async (req: Request, res: Response) => {
         const { email, password, redirectUrl, redirectAppId } = req.body;
         const { token, redirectUrlWithToken } = await authService.login(email, password, redirectUrl, redirectAppId);
         res.send({ token, redirectUrlWithToken });
     }));
 
-    app.delete('/user/delete', Authorize, asyncWrapper(async (req: Request, res: Response, next: NextFunction) => {
+    app.delete('/delete', Authorize, asyncWrapper(async (req: Request, res: Response) => {
         const result = await authService.deleteUser(req.query.id as string);
         res.send(result);
     }));
 
-    app.post('/user/logout', Authorize, asyncWrapper(async (req: Request, res: Response, next: NextFunction) => {
+    app.post('/logout', Authorize, asyncWrapper(async (req: Request, res: Response) => {
         const result = await authService.logout(req.body.appId, req.body.userId);
         res.status(200).json({ result});
     }));
 
-    app.post('/user/createAccount', Authorize, asyncWrapper(async (req: Request, res: Response, next: NextFunction) => {
+    app.post('/createAccount', Authorize, asyncWrapper(async (req: Request, res: Response) => {
         const result = await authService.createAccount(req.body, req.query.appId as string);
         res.send(result);
     }));
 }
-
---
+++ src/utils/auth.validation.ts
@@ -0,0 +1,100 @@
+import { RequestHandler } from 'express';
+import { body, validationResult } from 'express-validator';
+
+// Type the handler explicitly
+export const handleValidationErrors: RequestHandler = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        res.status(400).json({ errors: errors.array()[0].msg });
+        return;
+    }
+    next();
+};
+export const validateUserSignIn: RequestHandler[] = [
+    body('email')
+        .not()
+        .isEmpty()
+        .withMessage('Email is required')
+        .isEmail()
+        .withMessage('Invalid email')
+        .normalizeEmail(),
+    body('password')
+        .not()
+        .isEmpty()
+        .withMessage('Password is required')
+        .isStrongPassword()
+        .withMessage('Password must be at least 6 characters with an Upper case character, lower case character, symbol and digit.'),
+    handleValidationErrors
+];
+
+export const validateUserSignUp: RequestHandler[] = [
+    body('firstName')
+        .isString()
+        .withMessage('First name must be a string')
+        .isLength({ min: 2 })
+        .withMessage('First name is required'),
+    body('lastName')
+        .isString()
+        .withMessage('Last name must be a string')
+        .isLength({ min: 2 })
+        .withMessage('Last name is required'),
+    body('email')
+        .not()
+        .isEmpty()
+        .withMessage('Email is required')
+        .isEmail()
+        .withMessage('Invalid email')
+        .normalizeEmail(),
+    body('password')
+        .not()
+        .isEmpty()
+        .withMessage('Password is required')
+        .isStrongPassword()
+        .withMessage('Password must be at least 6 characters with an Upper case character, lower case character, symbol and digit.'),
+    handleValidationErrors
+];
+
+export const validateUpdateUserInfo: RequestHandler[] = [
+    body('firstName')
+        .isString()
+        .withMessage('First name must be a string')
+        .isLength({ min: 2 })
+        .withMessage('First name is required'),
+    body('lastName')
+        .isString()
+        .withMessage('Last name must be a string')
+        .isLength({ min: 2 })
+        .withMessage('Last name is required'),
+    body('email')
+        .not()
+        .isEmpty()
+        .withMessage('Email is required')
+        .isEmail()
+        .withMessage('Invalid email')
+        .normalizeEmail(),
+    body('accountStatus')
+        .not()
+        .isEmpty()
+        .withMessage('Account status is required'),
+    handleValidationErrors
+];
+
+export const validateEmail: RequestHandler[] = [
+    body('email')
+        .not()
+        .isEmpty()
+        .withMessage('Email is required')
+        .isEmail()
+        .withMessage('Invalid email'),
+    handleValidationErrors
+];
+
+export const validatePasswordReset: RequestHandler[] = [
+    body('password')
+        .not()
+        .isEmpty()
+        .withMessage('Password must be provided')
+        .isStrongPassword()
+        .withMessage('Password must be at least 6 characters with an Upper case character, lower case character, symbol and digit.'),
+    handleValidationErrors
+];
